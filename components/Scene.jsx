c7deb5cb347b9e743f888d980e8d6bcfa2a9931c
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-06-02 12:59:19 +0500

    updated loading screen

--- components/Scene.jsx
+++ components/Scene.jsx
@@ -2,12 +2,42 @@
 import { useStateStore } from "@/stores/store";
 import { DragControls, Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import React, { Suspense } from "react";
 import Ground from "./Ground";
 import { Planter } from "./Planter";
 
 const Scene = () => (
-  <div className="flex-grow w-4/5 -z-0">
+  <div className="flex-grow w-4/5 -z-0"><Suspense fallback={
+    <div className="w-full h-full bg-gray-300 bg-gradient-to-tl from-[#71a32f] via-green-200 to-transparent "><div className="loader absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="box box0">
+        <div></div>
+      </div>
+      <div className="box box1">
+        <div></div>
+      </div>
+      <div className="box box2">
+        <div></div>
+      </div>
+      <div className="box box3">
+        <div></div>
+      </div>
+      <div className="box box4">
+        <div></div>
+      </div>
+      <div className="box box5">
+        <div></div>
+      </div>
+      <div className="box box6">
+        <div></div>
+      </div>
+      <div className="box box7">
+        <div></div>
+      </div>
+      <div className="ground">
+        <div></div>
+      </div>
+    </div></div>
+  }>
     <Canvas camera={{ position: [100, 10, 0], zoom: 7 }}>
       {/* <Stats /> */}
       <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 3} />
@@ -17,7 +47,7 @@ const Scene = () => (
 
       <Environment preset="forest" />
     </Canvas>
-  </div>
+  </Suspense></div>
 );
 
 const Plants = () => {
