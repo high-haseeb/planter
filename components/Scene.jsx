3bb5326f2aa8251a1711d65e653f790a4df457d8
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-07-21 03:41:00 +0500

    added loader component

--- components/Scene.jsx
+++ components/Scene.jsx
@@ -1,53 +1,30 @@
 "use client";
 import { useStateStore } from "@/stores/store";
-import { DragControls, Environment, OrbitControls } from "@react-three/drei";
+import { DragControls, Environment, OrbitControls, useProgress, Html } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import Ground from "./Ground";
 import { Planter } from "./Planter";
 
+function Loader() {
+  const { progress } = useProgress();
+  return <Html center className="font-bold text-6xl w-screen flex items-center justify-center text-brGreen">{progress.toFixed(0)}% loaded</Html>;
+}
+
 const Scene = () => (
-  <div className="flex-grow w-4/5 -z-0"><Suspense fallback={
-    <div className="w-full h-full bg-gray-300 bg-gradient-to-tl from-[#71a32f] via-green-200 to-transparent "><div className="loader absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <div className="box box0">
-        <div></div>
-      </div>
-      <div className="box box1">
-        <div></div>
-      </div>
-      <div className="box box2">
-        <div></div>
-      </div>
-      <div className="box box3">
-        <div></div>
-      </div>
-      <div className="box box4">
-        <div></div>
-      </div>
-      <div className="box box5">
-        <div></div>
-      </div>
-      <div className="box box6">
-        <div></div>
-      </div>
-      <div className="box box7">
-        <div></div>
-      </div>
-      <div className="ground">
-        <div></div>
-      </div>
-    </div></div>
-  }>
+  <div className="flex-grow w-4/5 -z-0">
     <Canvas camera={{ position: [100, 10, 0], zoom: 7 }}>
-      {/* <Stats /> */}
-      <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 3} />
+      <Suspense fallback={<Loader />}>
+        {/* <Stats /> */}
+        <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 3} />
 
-      <Plants />
-      <Ground />
+        <Plants />
+        <Ground />
 
-      <Environment preset="forest" />
+        <Environment preset="forest" />
+      </Suspense>
     </Canvas>
-  </Suspense></div>
+  </div>
 );
 
 const Plants = () => {
@@ -56,20 +33,30 @@ const Plants = () => {
   return (
     <>
       {garden.garden.map((planter, index) => {
-        if(index > garden.maxQuantity) return null; 
+        if (index > garden.maxQuantity) return null;
         const planterIndex = index * 3;
         if (planterIndex >= garden.height * row) {
           row += 1;
         }
         console.log(garden.maxQuantity);
-        const xOffset = - row * 4 + garden.width / 2 ;
+        const xOffset = -row * 4 + garden.width / 2;
         let yOffset = planterIndex - row * garden.height;
         yOffset += garden.height / 2;
 
         return (
-          <>{index < garden.maxQuantity && <DragControls axisLock={"y"}>
-            <Planter position={[xOffset, 0.6, yOffset]} scale={planter.size * 0.4} color={planter.color} index={index} trolley={planter.trolley} />
-          </DragControls>}</>
+          <>
+            {index < garden.maxQuantity && (
+              <DragControls axisLock={"y"}>
+                <Planter
+                  position={[xOffset, 0.6, yOffset]}
+                  scale={planter.size * 0.4}
+                  color={planter.color}
+                  index={index}
+                  trolley={planter.trolley}
+                />
+              </DragControls>
+            )}
+          </>
         );
       })}
     </>
